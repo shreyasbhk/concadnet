02a39c4241202ae4a7c14579afb200426b90e46d
Shreyas Hukkeri <hshreyas12@gmail.com>
2017-12-30 00:26:42 -0800

    V1.1 of ConCaDNet after data corruption. The model includes validation methods and a new separate testing file that measures AUC's obtained by all saves of a model on the test set and returns the best performing model.

--- concadnet.py
+++ concadnet.py
@@ -4,53 +4,52 @@ from sklearn.metrics import confusion_matrix, roc_auc_score
 
 model_version = 1
 batch_size = 100
-learning_rate = 0.00001
+val_batch_size = 300
+learning_rate = 0.001
 num_epochs = 200
 
 image_dimensions = (100, 100)
 train_dataset_file = "../Data/train_"+str(image_dimensions[0]) + "x" + str(image_dimensions[1]) + ".tfrecords"
-val_dataset_file = "../Data/test_"+str(image_dimensions[0]) + "x" + str(image_dimensions[1]) + ".tfrecords"
+val_dataset_file = "../Data/val_"+str(image_dimensions[0]) + "x" + str(image_dimensions[1]) + ".tfrecords"
 test_dataset_file = "../Data/test_"+str(image_dimensions[0]) + "x" + str(image_dimensions[1]) + ".tfrecords"
 
 
-def print_progress(epoch, batch, loss, val_auc, val_loss):
-    print("Epoch: {}, Batch: {}, Training Loss: {:.4f}, Validation AUC: {:.2f} %, Validation "
-          "Loss: {:.4f}".format(epoch, batch, loss, val_auc*100, val_loss))
+def print_progress(epoch, batch, auc, loss, val_auc, val_loss):
+    print("Epoch: {}, Batch: {}, Training AUC: {:.2f} %, Training Loss: {:.4f}, Validation AUC: {:.2f} %, Validation "
+          "Loss: {:.4f}".format(epoch, batch, auc*100, loss, val_auc*100, val_loss))
 
 with tf.device("/GPU:0"):
     def convnet(x, keep_prob, reuse):
         with tf.variable_scope('ConvNet', reuse=reuse):
-            x = 5*(x/tf.reduce_max(x))
-            conv = conv2d(x, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv = conv2d(conv, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv = conv2d(conv, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv = conv2d(conv, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv1 = max_pool2d(conv, (3, 3), (2, 2))
+            x = 5 * (x / tf.reduce_max(x))
+            conv = conv2d(x, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv1 = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
 
-            conv = conv2d(x, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv = conv2d(conv, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv = conv2d(conv, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv = conv2d(conv, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv2 = max_pool2d(conv, (3, 3), (2, 2))
+            conv = conv2d(x, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv2 = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
 
             concat = tf.concat([conv1, conv2], axis=1)
+            concat = max_pool2d(concat, (3, 3), (2, 2))
 
-            conv = conv2d(concat, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv = conv2d(conv, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv = conv2d(conv, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv = conv2d(conv, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv1 = max_pool2d(conv, (3, 3), (2, 2))
+            conv = conv2d(concat, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv1 = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
 
-            conv = conv2d(concat, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv = conv2d(conv, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv = conv2d(conv, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv = conv2d(conv, 8, (3, 3), activation_fn=tf.nn.leaky_relu)
-            conv2 = max_pool2d(conv, (3, 3), (2, 2))
+            conv = conv2d(concat, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv2 = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
 
-            conv = tf.concat([conv1, conv2], axis=1)
+            concat = tf.concat([conv1, conv2], axis=1)
+            concat = max_pool2d(concat, (3, 3), (2, 2))
 
-            conv = flatten(conv)
-            conv = fully_connected(conv, 512, activation_fn=None)
+            conv = flatten(concat)
+            conv = fully_connected(conv, 256, activation_fn=None)
             conv = dropout(conv, keep_prob)
             conv = fully_connected(conv, 1, activation_fn=None)
         return conv
@@ -68,7 +67,7 @@ with tf.device("/cpu:0"):
         return image, label
     dataset = tf.data.TFRecordDataset(train_dataset_file)
     dataset = dataset.map(parser_function)
-    dataset = dataset.repeat(3)
+    dataset = dataset.repeat(1)
     dataset = dataset.shuffle(batch_size)
     dataset = dataset.batch(batch_size)
     iterator = dataset.make_initializable_iterator()
@@ -76,7 +75,7 @@ with tf.device("/cpu:0"):
     val_dataset = tf.data.TFRecordDataset(test_dataset_file)
     val_dataset = val_dataset.map(parser_function)
     val_dataset = val_dataset.repeat(1)
-    val_dataset = val_dataset.shuffle(batch_size)
+    val_dataset = val_dataset.shuffle(val_batch_size)
     val_dataset = val_dataset.apply(tf.contrib.data.batch_and_drop_remainder(batch_size))
     val_iterator = val_dataset.make_initializable_iterator()
 
@@ -99,17 +98,20 @@ with tf.Session() as sess:
         sess.run(iterator.initializer)
         batch = -1
         epoch_loss = 0
+        epoch_auc = 0
         val_loss = 0
         val_batch = -1
         while True:
             batch += 1
             try:
                 images, labels = sess.run(iterator.get_next())
-                _, loss = sess.run([train_op, loss_op], feed_dict={x: images,
+                _, preds, loss = sess.run([train_op, auc, loss_op], feed_dict={x: images,
                                                                    y: labels,
                                                                    keep_prob: 0.8})
                 epoch_loss += loss
-                if batch%30==0 and batch!=0:
+                #epoch_auc += roc_auc_score(labels, preds)
+                epoch_auc += preds[0]
+                if batch%10==0 and batch!=0:
                     sess.run(val_iterator.initializer)
                     val_batch = -1
                     val_loss = 0
@@ -119,19 +121,21 @@ with tf.Session() as sess:
                         val_batch += 1
                         try:
                             val_images, val_labels = sess.run(val_iterator.get_next())
-                            preds, loss= sess.run([make_sense_logits, loss_op], feed_dict={x:val_images,
+                            preds, loss= sess.run([auc, loss_op], feed_dict={x:val_images,
                                                                                            y: val_labels,
                                                                                            keep_prob: 1.0})
                             #tn, fp, fn, tp = confusion_matrix(val_labels, preds)
                             val_loss += loss
-                            val_auc += roc_auc_score(val_labels, preds)
+                            #val_auc += roc_auc_score(val_labels, preds)
+                            val_auc += preds[0]
                         except tf.errors.OutOfRangeError:
-                            print_progress(epoch, batch, epoch_loss/batch, val_auc/val_batch, val_loss/val_batch)
+                            print_progress(epoch, batch, epoch_auc/batch, epoch_loss/batch, val_auc/val_batch,
+                                           val_loss/val_batch)
                             break
             except tf.errors.OutOfRangeError:
                 if epoch%5==0:
                     _ = saver.save(sess, save_path=str("../Models/Breast_Cancer/model-"+str(model_version)),
                                    global_step=epoch)
                 break
-        if val_loss / val_batch > 2 * epoch_loss / batch:
+        if val_loss / val_batch > 4 * epoch_loss / batch:
             break
---
+++ concadnet_runner.py
@@ -0,0 +1,110 @@
+import tensorflow as tf
+from tensorflow.contrib.layers import conv2d, max_pool2d, flatten, dropout, fully_connected
+from sklearn.metrics import confusion_matrix, roc_auc_score
+import os
+
+model_version = 1
+batch_size = 100
+
+num_epochs = 200
+image_dimensions = (100, 100)
+test_dataset_file = "../Data/test_"+str(image_dimensions[0]) + "x" + str(image_dimensions[1]) + ".tfrecords"
+
+
+with tf.device("/GPU:0"):
+    def convnet(x, keep_prob, reuse):
+        with tf.variable_scope('ConvNet', reuse=reuse):
+            x = 5 * (x / tf.reduce_max(x))
+            conv = conv2d(x, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv1 = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+
+            conv = conv2d(x, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv2 = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+
+            concat = tf.concat([conv1, conv2], axis=1)
+            concat = max_pool2d(concat, (3, 3), (2, 2))
+
+            conv = conv2d(concat, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv1 = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+
+            conv = conv2d(concat, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+            conv2 = conv2d(conv, 16, (3, 3), activation_fn=tf.nn.leaky_relu)
+
+            concat = tf.concat([conv1, conv2], axis=1)
+            concat = max_pool2d(concat, (3, 3), (2, 2))
+
+            conv = flatten(concat)
+            conv = fully_connected(conv, 256, activation_fn=None)
+            conv = dropout(conv, keep_prob)
+            conv = fully_connected(conv, 1, activation_fn=None)
+        return conv
+
+with tf.device("/cpu:0"):
+    def parser_function(example_proto):
+        features = {
+            "image": tf.FixedLenFeature((), tf.string, default_value=""),
+            "label": tf.FixedLenFeature((), tf.int64, default_value=0)
+        }
+        parsed_features = tf.parse_single_example(example_proto, features)
+        image = tf.reshape(tf.decode_raw(parsed_features["image"], tf.uint16),
+                           [image_dimensions[0], image_dimensions[1], 1])
+        label = tf.reshape(tf.cast(parsed_features["label"], tf.int32), [1])
+        return image, label
+    dataset = tf.data.TFRecordDataset(test_dataset_file)
+    dataset = dataset.map(parser_function)
+    dataset = dataset.repeat(1)
+    dataset = dataset.batch(batch_size)
+    iterator = dataset.make_initializable_iterator()
+
+def test_model(epoch_number):
+    with tf.Session() as sess:
+        x = tf.placeholder(tf.uint16, shape=[None, image_dimensions[0], image_dimensions[1], 1], name="input")
+        y = tf.placeholder(tf.int32, shape=[None, 1], name="label")
+        keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+
+        test_logits = convnet(x, keep_prob, reuse=tf.AUTO_REUSE)
+        make_sense_logits = tf.sigmoid(test_logits)
+        auc = tf.metrics.auc(y, make_sense_logits)
+
+        saver = tf.train.Saver()
+        saver.restore(sess, save_path=str("../Models/Breast_Cancer/model-"+str(model_version)+"-"+str(epoch_number)))
+        sess.run(tf.group(tf.global_variables_initializer(), tf.local_variables_initializer()))
+        sess.run(iterator.initializer)
+        test_auc = 0
+        test_batch = -1
+        while True:
+            test_batch += 1
+            try:
+                images, labels = sess.run(iterator.get_next())
+                preds= sess.run(auc, feed_dict={x: images,
+                                                                         y: labels,
+                                                                         keep_prob: 1})
+                #test_auc += roc_auc_score(labels, preds)
+                test_auc += preds[0]
+                #print(roc_auc_score(labels, preds))
+                #print(model_num)
+            except tf.errors.OutOfRangeError:
+                return test_auc/test_batch
+
+if __name__ == "__main__":
+    max_accuracy_auc = 0
+    best_model_num = 0
+    for epoch in range(num_epochs):
+        model_num = epoch
+        if os.path.isfile(str("../Models/Breast_Cancer/model-"+str(model_version)+"-"+str(model_num)+".meta")):
+            auc = test_model(model_num)
+            if auc > max_accuracy_auc:
+                max_accuracy_auc = auc
+                best_model_num = model_num
+    print("Best Model AUC: ")
+    print(max_accuracy_auc)
+    print("Best Model: ")
+    print(best_model_num)
