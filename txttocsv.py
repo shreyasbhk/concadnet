ab03b77dbdaeca32028b8c0710ed87009e699960
Shreyas Hukkeri <hshreyas12@gmail.com>
2018-01-21 16:57:52 -0800

    Progress Update - Added functionality of saving rest of the details

--- txttocsv.py
+++ txttocsv.py
@@ -1,10 +1,12 @@
 import csv
 import ast
+import os
 
-model_version = 16
+model_version = 15
 run_number = 1
 
 filename = "../Models/"+str(model_version)+"/"+str(run_number)+"/Training_Progress.txt"
+csv_filename = "../Models/"+str(model_version)+"/"+str(run_number)+"/Training_Progress.csv"
 ranges_prefix = "../Models/"+str(model_version)+"/"+str(run_number)+"/"
 
 
@@ -20,9 +22,28 @@ def convert_and_save_ranges(ranges):
             writer.writerow(temp_arr)
 
 
+def convert_and_save_details(details):
+    temp_arr = [details[0][7:], details[1][7:], details[2][16:], details[3][15:], details[4][18:],
+                details[5][17:], details[6][15:], details[7][14:]]
+    print(temp_arr)
+    with open(csv_filename, "a+") as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(temp_arr)
+
+
+for i in range(5):
+    n = ranges_prefix+"Layer "+str(i)+".csv"
+    if os.path.isfile(n):
+        os.remove(n)
+
+if os.path.isfile(csv_filename):
+    os.remove(csv_filename)
+
 with open(filename) as f:
     lines = f.readlines()
     for line in lines:
         details = line.split(',')
         ranges = ','.join(details[8:58]).replace('\n', '')[15:]
         convert_and_save_ranges(ranges)
+        print(details[:8])
+        convert_and_save_details(details[:8])
